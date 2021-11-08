361c32a665b8a99654206f7a21cb11ba55322dd6
Zhaohang Yan <10969mart1n@gmail.com>
2021-11-08 15:55:19 +0800

    Implementing linear sum assignment as the one in scipy

--- C#/Program.cs
+++ C#/Program.cs
@@ -30,8 +30,24 @@ namespace C_
             //     new double[] { 7, 6, 3 },
             //     new double[] { 3, 6, 8 }
             // };
+
+            // double[][] XC = new double[][] {
+            //     new double[] {35.0456, -85.2672}, 
+            //     new double[] {35.1174, -89.9711}, 
+            //     new double[] {35.9728, -83.9422},
+            //     new double[] {36.1667, -86.7833}
+            // };
+
+            // double[][] XD = new double[][] {
+            //     new double[] {35.0456, -85.2672}, 
+            //     new double[] {35.1174, -89.9711}, 
+            //     new double[] {35.9728, -83.9422},
+            //     new double[] {36.1667, -86.7833},
+            //     new double[] {36.1667, -86.7833}
+            // };
             
-            // result = cDist(XB,XB);
+            
+            // var result = cDist(XC,XD);
             // for (int i = 0; i < 4; i++) {
             //     for (int j = 0; j < 4; j++) {
             //         Console.WriteLine(result[i,j]);
@@ -39,7 +55,13 @@ namespace C_
             // }
 
             // Testing LinearSumAssignment
-            double[,] cost = new double[,] { {4, 1, 3}, {2, 0, 5}, {3, 2, 2} };
+            double[,] cost = new double[,] { {4, 1, 3}, 
+                                             {2, 0, 5}, 
+                                             {3, 2, 2} };
+            // double[,] cost = new double[,] { {35.0456, -85.2672}, 
+            //                                  {35.1174, -89.9711}, 
+            //                                  {35.9728, -83.9422},
+            //                                  {36.1667, -86.7833} };
             var ret = LinearSumAssignment(cost);
             foreach (int[] ind in ret) {
                 foreach (int idx in ind) {
--- C#/multi-animal-tracking.cs
+++ C#/multi-animal-tracking.cs
@@ -13,18 +13,13 @@ namespace BonZeb {
 		// }
 
 		public static double distance(double[] A, double[] B) {
-
 			int n = A.Length;
-			
 			if (n != B.Length) return -1;
-
 			double ret = 0;
-			
 			for (int i = 0; i < n; i++) {
 				var temp = A[i] - B[i];
 				ret += Math.Pow(temp, 2);
 			}
-			
 			return Math.Sqrt(ret);
 		}
 	
@@ -33,17 +28,16 @@ namespace BonZeb {
 
 			/* Parameters
 				XA: array_like
-				An mA by n array of mA original observations in an n-dimensional space. Inputs are converted to double type.
+					An mA by n array of mA original observations in an n-dimensional space. Inputs are converted to double type.
 
 				XB: array_like
-			/	An mB by n array of mB original observations in an n-dimensional space. Inputs are converted to double type.
+					An mB by n array of mB original observations in an n-dimensional space. Inputs are converted to double type.
 
 			Returns
 				Y: ndarray
 				A mA by mB distance matrix is returned. For each i and j, the metric dist(u=XA[i], v=XB[j]) is computed and stored 
 				in the ij th entry.
 			*/
-
 			int mA = XA.Length, mB = XB.Length;
 
 			double[,] result = new double[mA, mB];
@@ -58,6 +52,21 @@ namespace BonZeb {
 		}
 
 		public static List<int[]> LinearSumAssignment(double[,] cost) {
+			/*
+				Parameters
+				----------
+				cost_matrix : array
+					The cost matrix of the bipartite graph.
+
+				Returns
+				-------
+				row_ind, col_ind : array
+					An array of row indices and one of corresponding column indices giving
+					the optimal assignment. The cost of the assignment can be computed
+					as ``cost_matrix[row_ind, col_ind].sum()``. The row indices will be
+					sorted; in the case of a square cost matrix they will be equal to
+					``numpy.arange(cost_matrix.shape[0])``.
+			*/
 
 			var nr = cost.GetLength(0);
             var nc = cost.GetLength(1);
@@ -76,169 +85,90 @@ namespace BonZeb {
             Array.Fill(y, -1);
 
 			// Find a matching one vertex at a time
-            for (var curRow = 0; curRow < nr; curRow++) {
-                double minVal = 0;
-                var i = curRow;
-                // Reset working arrays
-                var remaining = new int[nc].ToList();
-                var numRemaining = nc;
-                for (var jp = 0; jp < nc; jp++) {
-                    remaining[jp] = jp;
-                    shortestPathCosts[jp] = double.PositiveInfinity;
-                }
-                Array.Clear(sr, 0, sr.Length);
-                Array.Clear(sc, 0, sc.Length);
+			for (var curRow = 0; curRow < nr; curRow++) {
+				double minVal = 0;
+				var i = curRow;
+				// Reset working arrays
+				var remaining = new int[nc].ToList();
+				var numRemaining = nc;
+				for (var jp = 0; jp < nc; jp++) {
+						remaining[jp] = jp;
+						shortestPathCosts[jp] = double.PositiveInfinity;
+				}
+				Array.Clear(sr, 0, sr.Length);
+				Array.Clear(sc, 0, sc.Length);
 
-                // Start finding augmenting path
-                var sink = -1;
-                while (sink == -1) {
-                    sr[i] = true;
-                    var indexLowest = -1;
-                    var lowest = double.PositiveInfinity;
-                    for (var jk = 0; jk < numRemaining; jk++) {
-                        var jl = remaining[jk];
-                        // Note that this is the main bottleneck of this method; looking up the cost array
-                        // is costly. Some obvious attempts to improve performance include swapping rows and
-                        // columns, and disabling CLR bounds checking by using pointers to access the elements
-                        // instead. We do not seem to get any significant improvements over the simpler
-                        // approach below though.
-                        var r = minVal + cost[i, jl] - u[i] - v[jl];
-                        if (r < shortestPathCosts[jl])
-                        {
-                            path[jl] = i;
-                            shortestPathCosts[jl] = r;
-                        }
+				// Start finding augmenting path
+				var sink = -1;
+				while (sink == -1) {
+					sr[i] = true;
+					var indexLowest = -1;
+					var lowest = double.PositiveInfinity;
+					for (var jk = 0; jk < numRemaining; jk++) {
+						var jl = remaining[jk];
+						// Note that this is the main bottleneck of this method; looking up the cost array
+						// is costly. Some obvious attempts to improve performance include swapping rows and
+						// columns, and disabling CLR bounds checking by using pointers to access the elements
+						// instead. We do not seem to get any significant improvements over the simpler
+						// approach below though.
+						var r = minVal + cost[i, jl] - u[i] - v[jl];
+						if (r < shortestPathCosts[jl])
+						{
+							path[jl] = i;
+							shortestPathCosts[jl] = r;
+						}
 
-                        if (shortestPathCosts[jl] < lowest || shortestPathCosts[jl] == lowest && y[jl] == -1)
-                        {
-                            lowest = shortestPathCosts[jl];
-                            indexLowest = jk;
-                        }
-                    }
+						if (shortestPathCosts[jl] < lowest || shortestPathCosts[jl] == lowest && y[jl] == -1)
+						{
+							lowest = shortestPathCosts[jl];
+							indexLowest = jk;
+						}
+					}
 
-                    minVal = lowest;
-                    var jp = remaining[indexLowest];
-                    if (double.IsPositiveInfinity(minVal))
-                        throw new InvalidOperationException("No feasible solution.");
-                    if (y[jp] == -1)
-                        sink = jp;
-                    else
-                        i = y[jp];
+					minVal = lowest;
+					var jp = remaining[indexLowest];
+					if (double.IsPositiveInfinity(minVal))
+						throw new InvalidOperationException("No feasible solution.");
+					if (y[jp] == -1)
+						sink = jp;
+					else
+						i = y[jp];
 
-                    sc[jp] = true;
-                    remaining[indexLowest] = remaining[--numRemaining];
-                    remaining.RemoveAt(numRemaining);
-                }
+					sc[jp] = true;
+					remaining[indexLowest] = remaining[--numRemaining];
+					remaining.RemoveAt(numRemaining);
+				}
 
-                if (sink < 0)
-                    throw new InvalidOperationException("No feasible solution.");
+				if (sink < 0)
+					throw new InvalidOperationException("No feasible solution.");
 
-                // Update dual variables
-                u[curRow] += minVal;
-                for (var ip = 0; ip < nr; ip++)
-                    if (sr[ip] && ip != curRow)
-                        u[ip] += minVal - shortestPathCosts[x[ip]];
+				// Update dual variables
+				u[curRow] += minVal;
+				for (var ip = 0; ip < nr; ip++)
+					if (sr[ip] && ip != curRow)
+						u[ip] += minVal - shortestPathCosts[x[ip]];
 
-                for (var jp = 0; jp < nc; jp++)
-                    if (sc[jp])
-                        v[jp] -= minVal - shortestPathCosts[jp];
+				for (var jp = 0; jp < nc; jp++)
+					if (sc[jp])
+						v[jp] -= minVal - shortestPathCosts[jp];
 
-                // Augment previous solution
-                var j = sink;
-                while (true) {
-                    var ip = path[j];
-                    y[j] = ip;
-                    (j, x[ip]) = (x[ip], j);
-                    if (ip == curRow)
-                        break;
-                }
-            }
+				// Augment previous solution
+				var j = sink;
+				while (true) {
+					var ip = path[j];
+					y[j] = ip;
+					(j, x[ip]) = (x[ip], j);
+					if (ip == curRow)
+						break;
+				}
+			}
 			var ret = new List<int[]>();
+			Array.Sort(x);
 			ret.Add(x);
 			ret.Add(y);
 			return ret;
 		}
-
-		// def find_identities(first_tracking_data_arr, second_tracking_data_arr, tail_curvature_arr, n_fish, n_tracking_points):
-		//     cost = cdist(first_tracking_data_arr[:, 0], second_tracking_data_arr[:, 0])
-		//     result = linear_sum_assignment(cost)
-		//     if second_tracking_data_arr.shape[0] < n_fish:
-		//         missed_index = [i for i in range(len(first_tracking_data_arr)) if i not in result[0]][0]
-		//         merged_index = np.where(cost[missed_index] == np.min(cost[missed_index]))[0][0]
-		//         second_tracking_data_arr = np.append(second_tracking_data_arr, second_tracking_data_arr[merged_index]).reshape(-1, n_tracking_points, 2)
-		//         tail_curvature_arr = np.append(tail_curvature_arr, tail_curvature_arr[merged_index]).reshape(-1, n_tracking_points - 1)
-		//         second_tracking_data_arr, tail_curvature_arr, result = find_identities(first_tracking_data_arr, second_tracking_data_arr, tail_curvature_arr, n_fish, n_tracking_points)
-		//     return second_tracking_data_arr, tail_curvature_arr, result
-
-		// public Tuple FindIdentities(NDArray first_tracking_data_arr, NDArray second_tracking_data_arr, NDArray tail_curvature_arr, int n_fish, int n_tracking_points) {
-		// 	var cost = cdist(GetColumn(first_tracking_data_arr, 0), GetColumn(second_tracking_data_arr, 0));
-		// 	var result = LinearSumAssignment(cost);
-		// 	if (second_tracking_data_arr.Length < n_fish) {
-		// 		var missed_indices = new List<int>();
-		// 		for (int i = 0; i < first_tracking_data_arr.Length; i++) {
-		// 			if (!result.Item1.Contains(i)) {
-		// 				missed_indices.Add(i);
-		// 			}
-		// 		}
-		// 		var missed_index = missed_index[0];
-		// 		var merged_index = np.where(cost[missed_index] == np.min(cost[missed_index]))[0][0];
-		// 		second_tracking_data_arr = np.append(second_tracking_data_arr, second_tracking_data_arr[merged_index]).reshape(-1, n_tracking_points, 2);
-		// 		tail_curvature_arr = np.append(tail_curvature_arr, tail_curvature_arr[merged_index]).reshape(-1, n_tracking_points - 1);
-		// 		var temp_result = FindIdentities(first_tracking_data_arr, second_tracking_data_arr, tail_curvature_arr, n_fish, n_tracking_points);
-		// 		second_tracking_data_arr = temp_result.Item1;
-		// 		tail_curvature_arr = temp_result.Item2;
-		// 		result = temp_result.Item3;
-		// 	}
-
-		// 	return new Tuple(second_tracking_data_arr, tail_curvature_arr, result);
-		// }
-
-
-
-		// // def reorder_data_for_identities(tracking_data, tail_curvature, n_fish, n_tracking_points):
-		// // 	new_tracking_data = np.zeros((len(tracking_data), n_fish, n_tracking_points, 2))
-		// // 	new_tail_curvature = np.zeros((len(tail_curvature), n_fish, n_tracking_points - 1))
-		// // 	for i in range(len(tracking_data) - 1):
-		// // 	    if i == 0:
-		// // 	        first_tracking_data_arr = tracking_data[0]
-		// // 	        new_tracking_data[0] = tracking_data[0]
-		// // 	        new_tail_curvature[0] = tail_curvature[0]
-		// // 	    second_tracking_data_arr = tracking_data[i + 1]
-		// // 	    tail_curvature_arr = tail_curvature[i + 1]
-		// // 	    new_tracking_data_arr, new_tail_curvature_arr, new_order = find_identities(first_tracking_data_arr, second_tracking_data_arr, tail_curvature_arr, n_fish, n_tracking_points)
-		// // 	    new_tracking_data[i + 1] = new_tracking_data_arr[new_order[1]]
-		// // 	    new_tail_curvature[i + 1] = new_tail_curvature_arr[new_order[1]]
-		// // 	    first_tracking_data_arr = new_tracking_data[i + 1]
-		// // 	return [new_tracking_data, new_tail_curvature]
-
-		// public List ReorderDataForIdentities(NDArray tracking_data, NDArray tail_curvature, int n_fish, int n_tracking_points) {
-		// 	var new_tracking_data = np.zeros(tracking_data.Length, n_fish, n_tracking_points, 2);
-
-		// 	for (int i = 0; i < tracking_data.Length - 1; i++) {
-		// 		if (i == 0) {
-		// 			var first_tracking_data_arr = tracking_data[0];
-		// 			new_tracking_data[0] = tracking_data[0];
-		// 			new_tail_curvature[0] = tail_curvature[0];
-		// 		}
-							
-		// 		var second_tracking_data_arr = tracking_data[i + 1];
-		// 		var tail_curvature_arr = tail_curvature[i + 1];
-		// 		var temp_result = find_identities(first_tracking_data_arr, second_tracking_data_arr, tail_curvature_arr, n_fish, n_tracking_points);
-		// 		second_tracking_data_arr = temp_result.Item1;
-		// 		tail_curvature_arr = temp_result.Item2;
-		// 		result = temp_result.Item3;
-		// 		new_tracking_data[i + 1] = new_tracking_data_arr[new_order[1]];
-		// 		new_tail_curvature[i + 1] = new_tail_curvature_arr[new_order[1]];
-		// 		first_tracking_data_arr = new_tracking_data[i + 1];
-		// 	}
-					
-		// 	// var ret = new List();
-		// 	// ret.Add(new_tracking_data);
-		// 	// ret.Add(new_tail_curvature);
-		// 	return new List(){new_tracking_data, new_tail_curvature};
-		// }
 	}
-
 
 }
 
