f59aa0e71d0301024cb7ce30e82e65727c4e4c68
Zhaohang Yan <56213581+ymart1n@users.noreply.github.com>
2022-03-21 23:16:08 -0400

    predict twice

--- C#/Program.cs
+++ C#/Program.cs
@@ -7,21 +7,34 @@ namespace C_
     class Program
     {
 
-        public static void push_test()
+        public static async void push_test()
         {
             var rand = new Random();
 
             // Let's say we have the following sequence of coordinate points:
-            double[] x = new double[100];
-            x = x.Apply(x_i => rand.NextDouble() * 60);
-            double[] y = x.Apply(x_i => 0.25 * x_i * x_i + rand.NextDouble() * 600);
+            // double[] x = new double[100];
+            double[] x = Vector.Range(0.0, 50.0);
+            // x = x.Apply(x_i => rand.NextDouble() * 5);
+            double[] y = x.Apply(x_i => 0.07 * x_i * x_i);
 
             // Create a new Kalman filter
-            var kf = new KF2D(60, 0.0005f, 0.1f);
+            var kf = new KF2D(1, 0.1, 0.1f);
+            // var kf = new KF2D();
+
+            Console.WriteLine("Actual Position: ");
+
+            for (int i = 0; i < x.Length; i++) {
+                Console.WriteLine(x[i] + ", " + y[i]);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Estimated Position: ");
 
             // Push the points into the filter
-            for (int i = 0; i < x.Length; i++) {
-                kf.Push(x[i], y[i]);
+            for (int i = 1; i < x.Length; i++) {
+                kf.Push(x[i-1], y[i-1]);
+
+                kf.Push(kf.X, kf.Y);
 
                 // Estimate the points location
                 double newX = kf.X;
@@ -30,12 +43,12 @@ namespace C_
                 // Estimate the points velocity
                 double velX = kf.XAxisVelocity;
                 double velY = kf.YAxisVelocity;
-                Console.WriteLine("Current Position: " + x[i] + ", " + y[i]);
-                Console.WriteLine("Estimated Position: " + newX + ", " + newY);
-                Console.WriteLine("Velocity: " + velX + ", " + velY);
-                Console.WriteLine();
+                // Console.WriteLine("Actual Position: " + x[i] + ", " + y[i]);
+                // Console.WriteLine("Estimated Position: " + newX + ", " + newY);
+                // Console.WriteLine("Estimated Velocity: " + velX + ", " + velY);
+                // Console.WriteLine();
                 // Console.WriteLine(x[i] + ", " + y[i]);
-                // Console.WriteLine(newX + ", " + newY);
+                Console.WriteLine(newX + ", " + newY);
             }
                 
 
@@ -43,7 +56,6 @@ namespace C_
 
         static void Main(string[] args)
         {
-
             push_test();
         }
     }
